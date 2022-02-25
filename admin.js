bece056754f87a00593776c3cb14a6a42d5d15e7
amer <amermostaafa@gmail.com>
2022-02-25 18:30:26 +0200

    init

--- admin.js
+++ admin.js
@@ -4,6 +4,8 @@ var quill = new Quill("#editor", {
 
 const subBtn = document.getElementById("subBtn");
 const hideInput = document.getElementById("hide");
+const form = document.getElementById("main-form");
 subBtn.addEventListener("click", () => {
   hideInput.value = quill.root.innerHTML;
+  form.submit();
 });
